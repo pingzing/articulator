446f8dfa56dc9801a5a9e198665a983ebb0a9667
pingzing <neil.mcalister@futurice.com>
2016-04-08 11:47:48 +0300

    Fix build script, add early-return scripts  * Build script now copies contents of the scripts directory (minus the .gitignores) to the output dir.  * scripts dir is now gitignored properly  * any scripts located in the "ret_immediately" folder will now be cause the server to return a response immediately, then run the script after a 500ms delay

--- build.rs
+++ build.rs
@@ -6,8 +6,9 @@ use walkdir::WalkDir;
 use std::fs;
 
 pub fn main() {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
     let cargo_manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    println!("cargo:rerun-if-changed={:?}", cargo_manifest_dir);
+    let out_dir = env::var_os("OUT_DIR").unwrap();    
     let exe_dir = Path::new(out_dir.to_str().unwrap())
                       .parent().unwrap()
                       .parent().unwrap()
@@ -26,6 +27,9 @@ pub fn main() {
     let dst = Path::new(exe_dir);        
     let script_files = WalkDir::new(script_folder);
     
+    //remove old dir, if it exists
+    fs::remove_dir_all(dst.join(Path::new("scripts")));
+    
     //create desination dir(s)
     match fs::create_dir_all(dst.join(Path::new("scripts").join(Path::new("ret_immediately")))) {
         Ok(_) => {
@@ -33,6 +37,7 @@ pub fn main() {
                 if file.is_ok() {            
                     let file = file.unwrap();
                     if !file.file_type().is_file() {continue;}
+                    if file.file_name() == ".gitignore" {continue;}
                     let src = &file.path();
                     let prefixless_dest = src.strip_prefix(prefix_path).unwrap();
                     let dst = Path::new(&dst).join(&prefixless_dest);
--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,17 @@ use walkdir::WalkDir;
 use std::process::Command;
 use std::env;
 use std::io;
-use std::path::PathBuf;
 use std::fs;
-use std::fs::DirEntry;
+use std::thread;
+use std::time::Duration;
+use std::path::{Path, PathBuf};
 use rustc_serialize::json;
 use std::str::FromStr;
 
 mod json_structs;
 use json_structs::Script;
+
+static IMMEDIATE_RET_PATH : &'static str = "ret_immediately";
 
 fn main() {    
     let (logger_before, logger_after) = Logger::new(None);
@@ -34,7 +37,7 @@ fn main() {
     chain.link_before(logger_before);
     chain.link_after(logger_after);
 
-    Iron::new(chain).http("192.168.0.7:3000").unwrap();
+    Iron::new(chain).http("10.6.1.25:3000").unwrap();
 }
 
 // todo: eliminate the unwrap-infestation in here
@@ -42,19 +45,21 @@ fn show_scripts_handler(_: &mut Request) -> IronResult<Response> {
     println!("Getting scripts...");
     match get_script_folder() {
         Ok(folder) => {
-            let paths = WalkDir::new(folder);
+            let paths = WalkDir::new(&folder);
 
             let mut scripts = Vec::new();
             for p in paths {                
                 let p: walkdir::DirEntry = p.unwrap();
-                if !p.file_type().is_file() {continue};
+                if !p.file_type().is_file() {
+                    continue
+                };
                 let split_str = String::from(p.file_name().to_str().unwrap());
                 let split_str: Vec<&str> = split_str.split('.').collect();
                 let name = *split_str.first().unwrap();
                 let mut path = PathBuf::new();
                 path.push(p.path());
-                let path = path.strip_prefix(path.parent().unwrap().parent().unwrap()).unwrap();
-                let path = String::from(path.to_str().unwrap()).replace("\\", "/");
+                let path_root = Path::new(path.strip_prefix(folder.as_path().parent().unwrap()).unwrap());
+                let path = String::from(path_root.to_str().unwrap()).replace("\\", "/");
                 scripts.push(Script::new(String::from_str(name).unwrap(), String::from(path)));
             }
             let scripts = json::encode(&scripts).unwrap();
@@ -66,11 +71,26 @@ fn show_scripts_handler(_: &mut Request) -> IronResult<Response> {
 
 fn script_handler(req: &mut Request) -> IronResult<Response> {
     let ref query = req.extensions.get::<Router>().unwrap().find("scriptName").unwrap_or("/");
-    let script_path = get_script_path(query);
+    let script_path = get_script_path(query);    
 
     match script_path {
         Ok(path) => {
-            let output = Command::new("powershell.exe")
+            if path.to_string_lossy().contains(IMMEDIATE_RET_PATH) {
+                run_early_return_script(path)
+            }
+            else {
+                run_normal_script(path)
+            }            
+        }
+        Err(_) => {
+            println!("Could not find script.");
+            script_error_handler()
+        } 
+    }
+}
+
+fn run_normal_script(path: PathBuf) -> IronResult<Response> {
+    let output = Command::new("powershell.exe")
                              .arg("-executionpolicy")
                              .arg("bypass")
                              .arg("-File")
@@ -82,6 +102,7 @@ fn script_handler(req: &mut Request) -> IronResult<Response> {
                         true => {
                             let script_output = String::from_utf8_lossy(&output.stdout)
                                                     .into_owned();
+                            println!("Script success. Output:\n{}", script_output);
                             Ok(Response::with((status::Ok, script_output)))
                         }
                         false => {
@@ -95,13 +116,16 @@ fn script_handler(req: &mut Request) -> IronResult<Response> {
                     script_error_handler()
                 }                
             }
-        }
-        Err(_) => {
-            println!("Could not find script.");
-            script_error_handler()
-        } 
-    }
 }
+
+fn run_early_return_script(path: PathBuf) -> IronResult<Response> {
+    thread::spawn(|| {
+        thread::sleep(Duration::from_millis(500));
+        run_normal_script(path).ok(); //explicitly ignoring the Result. Can't do anything about it at this point
+    });
+    Ok(Response::with((status::Ok, "Attempted to kick off early-return script.")))
+}
+
 
 fn script_error_handler() -> IronResult<Response> {
     Ok(Response::with(status::InternalServerError))
@@ -118,7 +142,22 @@ fn get_script_folder() -> io::Result<PathBuf> {
 fn get_script_path(script_name: &str) -> io::Result<PathBuf> {
     let current_exe = try!(env::current_exe());
     match current_exe.parent() {
-        Some(parent_dir) => Ok(parent_dir.join("scripts").join(format!("{}.ps1", script_name))),
+        Some(parent_dir) => {
+            let script_path = parent_dir.join("scripts").join(format!("{}.ps1", script_name)); 
+            //check in scripts dir
+            if fs::metadata(&script_path).is_ok() {
+                Ok(script_path)
+            }
+            else {
+                let script_path = parent_dir.join("scripts").join(IMMEDIATE_RET_PATH).join(format!("{}.ps1", script_name));
+                if fs::metadata(&script_path).is_ok() {
+                    Ok(script_path)
+                }
+                else {
+                    Err(io::Error::new(io::ErrorKind::NotFound, "Unable to generate script path."))
+                }
+            }
+        },
         None => Err(io::Error::new(io::ErrorKind::NotFound, "Unable to generate script path.")),
     }
 }
