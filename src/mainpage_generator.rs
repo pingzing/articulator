2f5df69a39e52380cb52590f54a0a2e89af65aa9
pingzing <mcalistern@gmail.com>
2016-04-09 19:51:08 +0300

    Make generated mainpage a little more mobile-friendly

--- src/mainpage_generator.rs
+++ src/mainpage_generator.rs
@@ -12,16 +12,17 @@ impl MainPageHtml {
                 : raw!("<!DOCTYPE html>");
                 html {
                     head {
+                        meta(name="viewport", content="width=device-width, initial-scale=1.0"){}
                         title { : "Runnable scripts"}
                     }
-                    body {
-                        h1(id="main_header") {
+                    body(style="display: table") {
+                        h1(id="main_header", style="display: table") {
                             : "Runnable Scripts"
                         }
-                        h2(id="scripts_header") {
+                        h2(id="scripts_header", style="display: table") {
                             : "Scripts"
                         }
-                        ul(id="normal_scripts_list") {
+                        ul(id="normal_scripts_list", style="display: table") {
                             @ for script in scripts {
                                 li {
                                     : raw!(format!("<a href=\"/scr/{}\">{}</a>", script.get_name(), script.get_name()))
